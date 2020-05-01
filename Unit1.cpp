478203dcc7cea0fb3ff5d4f41d38519d709a9984
Marina Shisherina <cognitive.mmm@yandex.ru>
2020-05-01 10:40:46 +0300

    Все, кроме добавления текста и выбора, удаления элементов

--- Unit1.cpp
+++ Unit1.cpp
@@ -7,10 +7,12 @@
 #include "Unit2.h"
 #include <vector>
 #include <cmath>
+//#include <gdiplus.h>
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
+
 
 class GraphicElement {
 	virtual void create(int x1, int y1, TImage *Image1) = 0;
@@ -229,11 +231,135 @@ class MyCircle : public GraphicElement{
 		}
 
 };
+
+//--------Demo-----------------
+
+class MyText : public GraphicElement{
+	private:
+		String text;
+		TFont *font;
+		Graphics::TBitmap* gBitmap = new Graphics::TBitmap;
+		int x, y; //точка отрисовки bitmap на image
+
+		//Form1->Image1->Picture->Bitmap -> Width = Form2->Edit1->Text.ToInt();
+		//Form1->Image1->Picture->Bitmap -> Height = Form2->Edit2->Text.ToInt();
+	public:
+		MyText(TFont *font, int x, int y, std::vector<int>&sequence){
+		   this -> font = font;
+		   this -> x = x;
+		   this -> y = y;
+
+		   sequence.emplace_back(5);
+		}
+
+		void setFont(TFont *font){
+			 this->font = font;
+		}
+		/*
+		void createMyBitmap(TImage *Image1){
+			gBitmap->Width  = 60;
+			gBitmap->Height = 20;
+
+			// this -> text = "|";
+
+			gBitmap->Canvas->Brush->Style=bsClear;
+			gBitmap->Canvas->Font = font;
+			gBitmap->Canvas->TextOut(2,2,text.c_str());
+
+			//gBitmap->Canvas->Pen->Style = psDot;
+			//gBitmap->Canvas->Pen->Width = 1;
+		   //	gBitmap->Canvas->Pen->Color = clBlue;
+			gBitmap->Canvas->Brush->Style = bsClear;
+			gBitmap->Canvas->Pen->Style = psClear;
+			//gBitmap->Canvas->Rectangle(0, 0, gBitmap->Width, gBitmap->Height);
+			Image1->Canvas->Draw(x,y,gBitmap);
+			//Form1->Image1->Picture->Bitmap->FreeImage();
+		}  */
+
+		void createText(TImage *Image1, String s){
+
+			this->text = s;
+
+			//gBitmap-> FreeImage();
+			//gBitmap = NULL;
+
+			gBitmap->Canvas->Font = font;
+			gBitmap->Width  = gBitmap->Canvas->TextWidth(text+2);
+			gBitmap->Height = gBitmap->Canvas->TextHeight(text);
+
+			//gBitmap->Width  = 40;
+			//gBitmap->Height = 40;
+			gBitmap->Canvas->Brush->Color = clWhite;
+			////gBitmap->Canvas->Rectangle(0, 0, gBitmap->Width, gBitmap->Height);
+			gBitmap->Canvas->Brush->Style=bsClear;
+			gBitmap->Canvas->Font = font;
+			//TLogFont f = font;
+			//f.lfEscapement=90; //Угол вывода
+			//Bitmap->Canvas->Font->Handle = CreateFontIndirect(&f);
+			//gBitmap->Canvas->Font->Handle = lfEscapement=90;
+			//font.nOrientation = 90;
+			//gBitmap->Canvas->Font->Handle = nOrientation = 90
+			gBitmap->Canvas->TextOut(2,2,text.c_str());
+
+			//TGraphics g = CreateGraphics();
+			//g.RotateTransform(45); // поворот на 45 градусов
+			//g.DrawImageUnscaled(gBitmap, 0, 0);
+			//f.lfEscapement=0;
+			//gBitmap->Canvas->Font->Handle = CreateFontIndirect(&f);
+			//gBitmap->Canvas->Pen->Style = psDot;
+			//gBitmap->Canvas->Pen->Width = 1;
+			//gBitmap->Canvas->Pen->Color = clBlue;
+			gBitmap->Canvas->Pen->Style = psClear;
+			gBitmap->Canvas->Brush->Style = bsClear;
+			Image1->Canvas->Brush->Style = bsClear;
+			Image1->Canvas->Pen->Style = psClear;
+			gBitmap->Transparent = true;  // разрешаем прозрачность
+			gBitmap->TransparentMode = tmFixed; // для прозрачности будем использовать фиксированное значение цвета
+			gBitmap->TransparentColor = clWhite;
+			//gBitmap->Canvas->RotateTransform(45);
+			//gBitmap->Canvas->Rectangle(0, 0, gBitmap->Width, gBitmap->Height);
+			Image1->Canvas->Draw(x,y,gBitmap);
+			//Form1->Image1->Picture->Bitmap->FreeImage();
+		}
+
+		void createRect(int x1, int y1, TImage *Image1){
+
+		}
+
+		void create(int x, int y, TImage *Image1){
+
+		}
+
+		void create(TImage *Image1){
+            gBitmap->Canvas->Font = font;
+			gBitmap->Width  = gBitmap->Canvas->TextWidth(text+2);
+			gBitmap->Height = gBitmap->Canvas->TextHeight(text);
+
+			gBitmap->Canvas->Brush->Color = clWhite;
+			gBitmap->Canvas->Brush->Style=bsClear;
+			gBitmap->Canvas->Font = font;
+			gBitmap->Canvas->TextOut(2,2,text.c_str());
+
+			gBitmap->Canvas->Pen->Style = psClear;
+			gBitmap->Canvas->Brush->Style = bsClear;
+			Image1->Canvas->Brush->Style = bsClear;
+			Image1->Canvas->Pen->Style = psClear;
+			gBitmap->Transparent = true;  // разрешаем прозрачность
+			gBitmap->TransparentMode = tmFixed; // для прозрачности будем использовать фиксированное значение цвета
+			gBitmap->TransparentColor = clWhite;
+			Image1->Canvas->Draw(x,y,gBitmap);
+
+		}
+
+};
+//----------Demo----------------
+
 //-------------------------------------------------
 void redrawing(const std::vector<Segment>&segments,
 			   const std::vector<MyArc>&arcs,
 			   const std::vector<MyPolyline>&polylines,
 			   const std::vector<MyCircle>&circles,
+			   const std::vector<MyText>&strings,
 			   const std::vector<int>&sequence,
 			   TImage *Image1, String fileName){
 
@@ -248,7 +374,7 @@ void redrawing(const std::vector<Segment>&segments,
 		Form1->Image1->Canvas->Rectangle(0,0,Form1->Image1->Width, Form1->Image1->Height);
 	}
 
-	int i = 0, a = 0, p = 0, e = 0;
+	int i = 0, a = 0, p = 0, e = 0, s = 0;
 	for(int t : sequence){
 		 if(t==1){
 			Segment segment = segments.at(i);
@@ -270,6 +396,11 @@ void redrawing(const std::vector<Segment>&segments,
 			myel.create(Image1);
 			e++;
 		 }
+		 if(t==5) {
+			MyText mytxt = strings.at(s);
+			mytxt.create(Image1);
+			s++;
+		 }
 	}
 	/*for(Segment n : segments) //перерисовка отрезков
 		n.create(Image1);*/
@@ -279,11 +410,13 @@ void vectors_cleaning(std::vector<Segment>&segments,
 					  std::vector<MyArc>&arcs,
 					  std::vector<MyPolyline>&polylines,
 					  std::vector<MyCircle>&circles,
+					  std::vector<MyText>&strings,
 					  std::vector<int>&sequence){
    segments.clear();
    arcs.clear();
    polylines.clear();
    circles.clear();
+   strings.clear();
    sequence.clear();
 
   // for(auto i = segments.begin(); i != segments.end(); segments.erase(i));
@@ -293,7 +426,8 @@ std::vector<Segment> segments;
 std::vector<MyArc> arcs;
 std::vector<MyPolyline> polylines;
 std::vector<MyCircle> circles;
-std::vector<int> sequence; // порядок элементов 1 - отрезокб 2 -дуга 3-полилиния
+std::vector<MyText> strings; // массив с текстом
+std::vector<int> sequence; // порядок элементов 1 - отрезокб 2 -дуга 3-полилиния 4-окружность 5-текст
 TColor color; // активный цвет
 TColor color2 = clWhite; // цвет заливки
 int bold = 1; // активная толщина
@@ -301,6 +435,7 @@ int i = -1; // текущий индекс для вектора отрезков
 int a = -1; // текущий индекс для вектора дуг
 int p = -1; // текущий индекс для вектора полилиний
 int e = -1; // текущий индекс для вектора полилиний
+int s = -1; // текущий индекс для вектора текста
 int point=0; // первая или вторая точка отрезка
 int x,y; //начальная точка отрезка
 bool save = false; // показывает сохранен ли уже файл
@@ -309,6 +444,7 @@ int seg_arc = 1;
 int x2=0, y2=0; //конец предыдущего сегмента полилинии и начало нового
 int click=0;
 int seg_arc_pr = 1;
+TFont *font;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
@@ -334,7 +470,8 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 	SpeedButton8 -> Down = false;
 	ComboBox1->ItemIndex = 0;
 	FlowPanel2-> Visible = false;
-    FlowPanel3->Visible = false;
+	FlowPanel3->Visible = false;
+	Panel3->Visible = false;
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::New1Click(TObject *Sender)
@@ -366,7 +503,7 @@ void __fastcall TForm1::New1Click(TObject *Sender)
 	else{
 		Form2->Show();
 		Form1->Enabled = false;
-		vectors_cleaning(segments, arcs, polylines, circles, sequence);   i=-1;
+		vectors_cleaning(segments, arcs, polylines, circles, strings, sequence);   i=-1;
 	}
 
 	save = false;
@@ -389,7 +526,7 @@ void __fastcall TForm1::Image1MouseMove(TObject *Sender, TShiftState Shift, int 
 	//----отрезок--------
 	if(SpeedButton3 -> Down == true){
 	 if(point == 1){
-		redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName); // перерисовываем все элементы
+		redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName); // перерисовываем все элементы
 		segments.at(i).setColor(color);
 		segments.at(i).setBold(bold);
 		segments.at(i).create(X,Y,Image1); // дорисовываем новый отрезок
@@ -400,13 +537,13 @@ void __fastcall TForm1::Image1MouseMove(TObject *Sender, TShiftState Shift, int 
     //----дуга--------
 	if(SpeedButton6 -> Down == true){
 		if(point == 1){
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			arcs.at(a).setColor(color);
 			arcs.at(a).setBold(bold);
 			arcs.at(a).create(X,Y);
 		}
 		if(point == 2){
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			arcs.at(a).setColor(color);
 			arcs.at(a).setBold(bold);
 			arcs.at(a).create(X,Y,Image1);
@@ -418,13 +555,13 @@ void __fastcall TForm1::Image1MouseMove(TObject *Sender, TShiftState Shift, int 
 
 	if(SpeedButton4 -> Down == true){
 		if(point == 1){
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			polylines.at(p).setColor(color);
 			polylines.at(p).setBold(bold);
 			polylines.at(p).create(X,Y);
 		}
 		if(point == 2){
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			polylines.at(p).setColor(color);
             polylines.at(p).setBold(bold);
 			polylines.at(p).create(X,Y,Image1);
@@ -438,13 +575,23 @@ void __fastcall TForm1::Image1MouseMove(TObject *Sender, TShiftState Shift, int 
 	if(SpeedButton5 -> Down == true){
 
 	 if(point == 1){
-		redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName); // перерисовываем все элементы
+		redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName); // перерисовываем все элементы
 		circles.at(e).setColor(color);
 		circles.at(e).setBold(bold);
 		circles.at(e).create(X,Y,Image1); //
 	 }
 	}
 	//----окружность end----
+
+    //----текст--------
+	if(SpeedButton7 -> Down == true){
+
+		if(point == 1){
+		  // strings.at(s).createMyBitmap(X, Y, Image1);
+		}
+
+	}
+	//----текст end----
 }
 //---------------------------------------------------------------------------
 void __fastcall TForm1::Image1MouseLeave(TObject *Sender)
@@ -509,7 +656,7 @@ void __fastcall TForm1::Close1Click(TObject *Sender)
 		Form1->Save1->Enabled = false;
 		Form1->SaveAs1->Enabled = false;
 		Form1->Close1->Enabled = false;
-		vectors_cleaning(segments, arcs, polylines, circles, sequence);   i=-1;
+		vectors_cleaning(segments, arcs, polylines, circles, strings, sequence);   i=-1;
         save = false;
 		fileName = "";
 	}
@@ -564,7 +711,7 @@ void __fastcall TForm1::Open1Click(TObject *Sender)
 		Form1->Enabled = true;
 		Form1->SaveAs1->Enabled = true;
 		Form1->Close1->Enabled = true;
-		vectors_cleaning(segments, arcs, polylines, circles, sequence);   i=-1;
+		vectors_cleaning(segments, arcs, polylines, circles, strings,  sequence);   i=-1;
 	}
 
 }
@@ -586,7 +733,7 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 
 		else if(point == 1){
 			point = 0;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 		}
 
 		if(save == true)
@@ -604,13 +751,13 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 
 		else if(point == 1){
 			point = 2;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			arcs.at(a).create(X,Y);
 		}
 
 		else if(point == 2){
 			point = 0;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 		}
 
 		if(save == true)
@@ -633,7 +780,7 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 		}
 
 		else if(point == 1){
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			polylines.at(p).create(X,Y);
 			if(seg_arc == 2) point = 2;
 			else {
@@ -645,7 +792,7 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 
 		else if(point == 2){
 			point = 1;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 			polylines.at(p).myAdd(color, bold, x2, y2);
 			//polylines.at(p).create(X,Y,2);
 		}
@@ -656,7 +803,7 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
         if ( Button == mbRight )
 		{
 			point = 0;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 		}
 	}
 
@@ -673,7 +820,7 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 
 		else if(point == 1){
 			point = 0;
-			redrawing(segments,arcs,polylines,circles,sequence,Image1,fileName);
+			redrawing(segments,arcs,polylines,circles,strings,sequence,Image1,fileName);
 		}
 
 		if(save == true)
@@ -681,6 +828,32 @@ void __fastcall TForm1::Image1MouseDown(TObject *Sender, TMouseButton Button, TS
 
 	}
 	//----окружность end----
+
+	//----текст--------
+	if(SpeedButton7 -> Down == true){
+	  /*
+		if(point == 0){
+			point = 1;
+			s++;
+			strings.emplace_back(MyText(font, X, Y, sequence));
+			strings.at(s).createText(Image1, Edit2->Text);
+		}
+
+		else if(point == 1){
+			point = 0;
+
+		}
+
+		if(save == true)
+			Form1->Save1->Enabled = true;
+		  */
+		  if(Edit2->Text!=""){
+			s++;
+			strings.emplace_back(MyText(font, X, Y, sequence));
+			strings.at(s).createText(Image1, Edit2->Text);
+		  }
+	}
+	//----текст end----
 }
 //---------------------------------------------------------------------------
 
@@ -696,6 +869,8 @@ void __fastcall TForm1::SpeedButton3Click(TObject *Sender)
 {
 	FlowPanel2->Visible = true;
 	FlowPanel3->Visible = false;
+	Panel3->Visible = false;
+	Panel2->Visible = true;
 
 }
 //---------------------------------------------------------------------------
@@ -744,6 +919,9 @@ void __fastcall TForm1::SpeedButton6Click(TObject *Sender)
 {
 	FlowPanel2->Visible = true;
 	FlowPanel3->Visible = false;
+	Panel3->Visible = false;
+	Panel2->Visible = true;
+	//FlowPanel4->Visible = false;
 }
 //---------------------------------------------------------------------------
 
@@ -752,6 +930,9 @@ void __fastcall TForm1::SpeedButton4Click(TObject *Sender)
 	point = 0;
 	FlowPanel2->Visible = true;
 	FlowPanel3->Visible = true;
+	Panel3->Visible = false;
+	Panel2->Visible = true;
+	//FlowPanel4->Visible = false;
 }
 //---------------------------------------------------------------------------
 
@@ -768,11 +949,125 @@ void __fastcall TForm1::FlowPanel3MouseDown(TObject *Sender, TMouseButton Button
 
 void __fastcall TForm1::SpeedButton5Click(TObject *Sender)
 {
-    point = 0;
+	point = 0;
 	FlowPanel2->Visible = true;
 	FlowPanel3->Visible = false;
+	Panel3->Visible = false;
+	Panel2->Visible = true;
+	//FlowPanel4->Visible = false;
 }
 //---------------------------------------------------------------------------
 
 
+
+void __fastcall TForm1::SpeedButton7Click(TObject *Sender)
+{
+	point = 0;
+	FlowPanel2->Visible = true;
+	FlowPanel3->Visible = false;
+	Panel3->Visible = true;
+	Panel2->Visible = false;
+	FlowPanel2-> Visible = false;
+    FontDialog1->Execute();
+	font = FontDialog1->Font;
+	//FlowPanel4->Visible = true;
+  /*
+	//Form1->DoubleBuffered = true;
+  Canvas->Rectangle(0,0,ClientWidth,ClientHeight);
+  char *s1 = "Привет,";
+// String s1 = "Hellow,";
+  String s2 = "Win32 API!";
+  Canvas->Brush->Style = bsClear;
+  HDC hdc = Canvas->Handle;
+
+  LOGFONT lf;
+  ZeroMemory(&lf, sizeof(LOGFONT));
+  lf.lfHeight = 20; // высота шрифта
+  lf.lfWidth = 20; // ширина символов
+  lf.lfWeight = 500; // толщина, "жирность" шрифта
+  lf.lfItalic = 1; // если это поле не равно 0, шрифт будет курсивный
+  lf.lfUnderline = 0; // если это поле не равно 0, шрифт будет подчёркнутый
+  lf.lfEscapement = 0; // 0; //шрифт без поворота
+  lf.lfCharSet = DEFAULT_CHARSET;
+  lf.lfPitchAndFamily = FIXED_PITCH; // DEFAULT_PITCH;
+  //wcscpy(lf.lfFaceName, _T("Courier New")); //    #include <tchar.h>
+ //wcscpy(lf.lfFaceName, UnicodeString("Courier New").c_str()); // "Times New Roman"
+// wcscpy(lf.lfFaceName, String("Courier New").c_str()); // "Times New Roman"
+ //strcpy(lf.lfFaceName, "Times New Roman");
+  Image1->Canvas->Font->Handle = CreateFontIndirect(&lf);
+//  Canvas->Brush->Color = clBlue;
+Image1->Canvas->Brush->Style=bsClear;
+  Image1->Canvas->FillRect(Canvas->ClipRect);
+//
+  SetTextAlign(hdc, TA_LEFT); // |TA_BASELINE|TA_UPDATECP);
+  int WidthOfString = Canvas->TextWidth(s1);
+  int HeightOfString = Canvas->TextHeight(s1);
+  int WidthOfString2 = Canvas->TextWidth(s2);
+  int HeightOfString2 = Canvas->TextHeight(s2);
+  SetTextColor(hdc, 0xff0000); //
+//  SetBkColor(hdc, 0xff00ff); // RGB(255,0,255)); //второй параметр задаёт цвет
+  SetBkMode(hdc, 0); // прозрачный фон
+//
+  Image1->Canvas->Brush->Style=bsClear;
+  Image1->Canvas->Font = font;
+  Image1->Canvas->TextOut(50,
+		  50, // отступ строк
+		  s2.c_str());
+
+		  */
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+   //	FontDialog1->Execute();
+	//font = FontDialog1->Font;
+	//LOGFONT f;
+   //	f = FontDialog1->ChooseFont();
+   //LOGFONT f;
+  // CHOOSEFONT ch;
+   //FontDialog1->Execute();
+  // ChooseFont();
+  //CHOOSEFONT fch;
+   HWND hwndParent; HFONT Font1; LOGFONT Logfont;
+  static LOGFONT lfResult;
+    ZeroMemory(&lfResult, sizeof(LOGFONT));
+	lfResult=Logfont;
+
+    CHOOSEFONT cf;
+    ZeroMemory(&cf, sizeof(CHOOSEFONT));
+    cf.lStructSize  = sizeof(CHOOSEFONT);
+    cf.hDC          = GetDC(hwndParent);
+    cf.Flags        = CF_SCREENFONTS|CF_INITTOLOGFONTSTRUCT;
+	cf.lpLogFont    = Logfont;
+    cf.nFontType    = SCREEN_FONTTYPE;
+    cf.hwndOwner    = hwndParent;
+
+    if(IDOK==ChooseFont(&cf) )
+    {
+		DeleteObject(Font1);
+		Font1=CreateFontIndirect(Logfont);
+		if(!Font1)
+			lfResult=Logfont;
+        if(hwndParent)
+            InvalidateRect(hwndParent, NULL, TRUE);
+    }
+
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	 // s++;
+	  //strings.emplace_back(MyText(font, X, Y, sequence));
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Edit2Change(TObject *Sender)
+{
+	 //strings.at(s).createText(Image1, Edit2->Text);
+}
+//---------------------------------------------------------------------------
 
